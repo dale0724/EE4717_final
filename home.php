3b894b79e66a9f4f9bc5a17eecf5006e4a940834
dale0724 <42413229+dale0724@users.noreply.github.com>
2021-11-02 12:09:14 +0800

    added theMovie Page

--- home.php
+++ home.php
@@ -16,20 +16,8 @@
     </header>
     <div class="wrapper functionArea">
         <div class="searchBox">
-            <form class="searchForm">
+            <form class="searchForm" method="POST" action="search.php">
                 <input type="text" placeholder="name" id="movieName">
-                <select name="cars" id="cars" aria-placeholder="Time Slot">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
-                </select>
-                <select name="cars" id="cars" aria-placeholder="Time Slot">
-                    <option value="volvo">Volvo</option>
-                    <option value="saab">Saab</option>
-                    <option value="mercedes">Mercedes</option>
-                    <option value="audi">Audi</option>
-                </select>
                 <button type="submit" id="searchButton">Search</button>
             </form>
         </div>
@@ -42,63 +30,46 @@
 
     <div class="wrapper">
         <h1 style="text-align: center; margin-bottom: 20px;">Now Showing</h3>
-        <?php
+            <?php
             $servername = "localhost";
             $username = "f32ee";
             $password = "f32ee";
             $dbname = "f32ee";
-            
+
             // Create connection
             $conn = mysqli_connect($servername, $username, $password, $dbname);
             // Check connection
             if (!$conn) {
                 die("Connection failed: " . mysqli_connect_error());
             }
-            
+
             $sql = " SELECT MovieID,MovieName,Poster FROM Movies";
             $result = mysqli_query($conn, $sql);
             if (mysqli_num_rows($result) > 0) {
                 // output data of each row
                 $count = 0;
                 while ($row = mysqli_fetch_assoc($result)) {
-                    if($count%3==0){
+                    if ($count % 3 == 0) {
                         echo '<div class="row">';
                     }
                     echo '<form id="'.$row["MovieID"].'" class="rowEntry" method="post" action="theMovie.php">';
-                    echo '<input type="hidden" name="MovieID" value="'.$row["MovieID"].'"/>';
-                    echo '<a onclick="document.getElementById("'.$row["MovieID"].'").submit();" class="entryImage"><img src="'.$row["Poster"].'" alt="" /></a>';
+                    echo '<input type="hidden" name="MovieID" value="'.$row["MovieID"].'">';
+                    echo '<a onclick="document.getElementById(\''.$row["MovieID"].'\').submit();" class="entryImage"><img src="'.$row["Poster"].'" alt="" /></a>';
                     echo '<h3>'.$row["MovieName"].'</h3>';
                     echo '</form>';
                     $count = $count + 1;
-                    if($count%3==0){
+                    if ($count % 3 == 0) {
                         echo '</div>';
                     }
-                    $movieName = $row["MovieName"];
-                    foreach($row as $key=>$val){
-                        echo $key.": ".$val."<br>";
-                    }
-                    
+                }
+                if($count%3!=0){
+                    echo '</div>';
                 }
             } else {
                 echo "sql error";
             }
             mysqli_close($conn);
-        ?>
-            <!-- <div class="row">
-                <form id="theForm" class="rowEntry" method="post" action="theMovie.php">
-                    <input type="hidden" name="name" value="value" /> 
-                    <a onclick="document.getElementById('theForm').submit();" class="entryImage"><img src="pics/blackPanther.jpg" alt="" /></a>
-                    <h3>No Time To Die</h3>
-                </form>
-                <section class="rowEntry">
-                    <a href="#" class="entryImage"><img src="pics/blackPanther.jpg" alt="" /></a>
-                    <h3>No Time To Die</h3>
-                </section>
-                <section class="rowEntry">
-                    <a href="#" class="entryImage"><img src="pics/blackPanther.jpg" alt="" /></a>
-                    <h3>No Time To Die</h3>
-                </section>
-            </div> -->
+            ?>
     </div>
 </body>
 
--- theMovie.php
+++ theMovie.php
@@ -1,42 +1,74 @@
-<?php
-$servername = "localhost";
-$username = "f32ee";
-$password = "f32ee";
-$dbname = "f32ee";
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = " SELECT * FROM Movies";
-$result = mysqli_query($conn, $sql);
-if (mysqli_num_rows($result) > 0) {
-    // output data of each row
-    while ($row = mysqli_fetch_assoc($result)) {
-        echo "result fetch error<br>";
-        $movieName = $row["MovieName"];
-        foreach($row as $key=>$val){
-            echo $key.": ".$val."<br>";
-        }
-        
-    }
-} else {
-    echo "sql error";
-}
-mysqli_close($conn);
-?>
 <!DOCTYPE html>
 <html>
 
 <head>
-    <title><?php $movieName ?></title>
+    <title>Movie</title>
+    <meta charset="utf-8" />
+    <link rel="stylesheet" href="myStyle.css" />
 </head>
 
-<body>
-    <?php echo $movieName ?>
+<body style="margin: 0;">
+    <header class="topnav">
+        <img src="pics/logo.png">
+        <a href="#contact">Upcoming</a>
+        <a href="#news">Now Showing</a>
+        <a class="active" href="#home">Home</a>
+    </header>
+    <div class="introduction wrapper">
+        <?php
+        $servername = "localhost";
+        $username = "f32ee";
+        $password = "f32ee";
+        $dbname = "f32ee";
+        $target = $_POST["MovieID"];
+
+        // Create connection
+        $conn = mysqli_connect($servername, $username, $password, $dbname);
+        // Check connection
+        if (!$conn) {
+            die("Connection failed: " . mysqli_connect_error());
+        }
+
+        $sql = " SELECT * FROM Movies where MovieID=$target";
+        $result = mysqli_query($conn, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            echo '<div class="poster">';
+            echo '<img src="' . $row["Poster"] . '">';
+            echo '</div>';
+            echo '<div class="information">';
+            echo '<dl>';
+            echo '<dt>Name:</dt>';
+            echo '<dd>' . $row["MovieName"] . '</dd>';
+            echo '<dt>Actors:</dt>';
+            echo '<dd>' . $row["Actors"] . '</dd>';
+            echo '<dt>Directors:</dt>';
+            echo '<dd>' . $row["Actors"] . '</dd>';
+            echo '<dt>Release Time:</dt>';
+            echo '<dd>' . $row["ReleaseTime"] . '</dd>';
+            echo '<dt>Descriptions:</dt>';
+            echo '<dd>' . $row["Descriptions"] . '</dd>';
+            echo '</dl>';
+            echo '</div>';
+            echo '<div class="trailer">';
+            echo $row["Trailer"];
+            echo '</div>';
+        } else {
+            echo "sql error";
+        }
+        mysqli_close($conn);
+        ?>
+        <form id="bookForm" method="POST" action="seat.php">
+            <label for="timeslot">Choose a time:</label>
+            <select id="timeSlot" name="timeList" form="bookForm">
+                <option value="Morning">Morning</option>
+                <option value="Afternoon">Afternoon</option>
+                <option value="Evening">Evening</option>
+            </select>
+            <input type="submit" value="Book!">
+        </form>
+    </div>
+
 </body>
 
 </html>
